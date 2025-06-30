9bbd8bf6dcd98e6c3d89991e28fa2391a34fed05
mehrdadfbx <mehrdadfbx1@gmail.com>
2025-06-30 02:16:17 -0700

    strings file add

---
+++ lib/core/constants/strings.dart
@@ -0,0 +1,49 @@
+class AppStrings {
+  static const appName = 'app_name';
+  static const transactions = 'transactions';
+  static const addTransaction = 'add_transaction';
+  static const editTransaction = 'edit_transaction';
+  static const deleteTransaction = 'delete_transaction';
+  static const noTransactions = 'no_transactions';
+
+  static const title = 'title';
+  static const amount = 'amount';
+  static const category = 'category';
+  static const date = 'date';
+  static const save = 'save';
+  static const cancel = 'cancel';
+  static const confirm = 'confirm';
+  static const back = 'back';
+
+  static const enterTitle = 'enter_title';
+  static const enterAmount = 'enter_amount';
+  static const selectCategory = 'select_category';
+  static const selectDate = 'select_date';
+
+  static const income = 'income';
+  static const expense = 'expense';
+
+  static const food = 'food';
+  static const transport = 'transport';
+  static const entertainment = 'entertainment';
+  static const shopping = 'shopping';
+  static const other = 'other';
+
+  static const dashboard = 'dashboard';
+  static const totalIncome = 'total_income';
+  static const totalExpense = 'total_expense';
+  static const balance = 'balance';
+
+  static const settings = 'settings';
+  static const darkMode = 'dark_mode';
+  static const language = 'language';
+  static const changeLanguage = 'change_language';
+  static const logout = 'logout';
+  static const aboutUs = 'about_us';
+
+  static const errorEmptyFields = 'error_empty_fields';
+  static const errorInvalidAmount = 'error_invalid_amount';
+  static const confirmDelete = 'confirm_delete';
+  static const yes = 'yes';
+  static const no = 'no';
+}
